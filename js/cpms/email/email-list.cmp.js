8f550d55020abc19658cf1fc2c7dffbd723aa8dd
Nadav sabah <nadav1sabah@gmail.com>
2019-06-20 16:16:24 +0300

    connect the details

--- js/cpms/email/email-list.cmp.js
+++ js/cpms/email/email-list.cmp.js
@@ -1,7 +1,36 @@
+import emailPreview from './email-preview.cmp.js'
+
 export default {
     template: `
-    <section class="about">
-        <h1>Email List</h1>
-    </section>
-    `
+    <section>
+    <h1>emails</h1>
+    <ul class="email-list">
+        <email-preview
+         v-for="(currentEmail) in emails" 
+         v-bind:key="currentEmail.id"
+          v-bind:email="currentEmail"
+          >
+         {{emails}}
+        </email-preview>
+      
+    </ul>
+</section>
+    `,
+    props: ['emails'],
+    mounted(){
+console.log('in the mounted',this.emails)
+    },
+    data(){
+        return{
+            
+        }
+    },
+    created(){
+console.log('created in the list')
+console.log('emails',this.emails)
+
+    },
+    components:{
+        emailPreview
+    },
 }
--- js/pages/email-details.cmp.js
+++ js/pages/email-details.cmp.js
@@ -5,31 +5,18 @@ import {
 
 export default {
     template: `
-    <section v-if="book">
-                <div class="book-in-details" >
-                    <button> back to email list</button>
-                <h1>Email Details</h1>
-                    <h1>title: {{email.title}}</h1>
-                    <h2> {{getPageCount}} , {{getpublishedDate}}</h2>
-                    <long-text v-bind:txt="book.description"></long-text>
-                    <img  v-bind:src="book.thumbnail"/>
-                    <h2 :class="priceClass" >Price: {{book.listPrice.amount}}{{ getCurrencyIcon}}</h2>
-                <h3 class = "sale">{{bookOnSale}}</h3>
-                </div>
-                <review-add :book="book"></review-add>
+    <section >
+                <h1>email details</h1>
         </section>
     `,
     data() {
         return {
-            book: null,
+         
 
         }
     },
     created() {
-        console.log('params:', this.$route.params.bookId);
-        const bookId = this.$route.params.bookId;
-        bookService.getById(bookId)
-            .then(book => this.book = book) 
+ 
         },
         
         mounted(){
@@ -37,71 +24,23 @@ export default {
     },
     computed: {
 
-        invalid() {
-            return !this.car.desc || this.car.checkedNames.length === 0
-        },
+       
 
-        bookOnSale() {
-            var isOnSale = this.book.listPrice.isOnSale
-            console.log(isOnSale)
-            if (isOnSale) {
-                return 'sale!!!'
-            } else {
-                return ''
-            }
-        },
+      
 
 
+      
 
-        priceClass() {
-            if (this.book.listPrice.amount >= 150) {
-                return 'expensive'
-            } else if (this.book.listPrice.amount <= 20) {
-                return 'cheap'
-            }
-        },
+        
 
-        getpublishedDate() {
-            var publishedDate = this.book.publishedDate
-            console.log(publishedDate)
-            if (publishedDate <= 2009) {
-                return 'Veteran Book'
-            } else if (publishedDate >= 2018) {
-                return 'new'
-            }
-        },
+        
 
-        getPageCount() {
-            var pageCount = this.book.pageCount
-            if (pageCount >= 500) {
-                return 'long reading'
-            } else if (pageCount < 500 && pageCount >= 200)
-                return 'Decent Reading'
-            else if (pageCount < 200 && pageCount >= 100)
-                return 'Light Reading'
-                else return 'Short Reading'
-        },
-
-        getCurrencyIcon() {
-            var currencyCode = this.book.listPrice.currencyCode
-            if (currencyCode === 'EUR') {
-                return '€'
-            } else if (currencyCode === 'ILS') {
-                return '₪'
-            } else {
-                return '$'
-            }
-        }
+        
     },
 
     methods: {
-        removeDisplay() {
-            this.$emit('hide');
-        }
+        
     },
-    components: {
-        longText,
-        reviewAdd
-    }
+    
 
 }

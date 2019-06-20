08514d5af2f64108cbc3fc7dba5a75ab11c8b5cc
chen mordechai <chen100030@gmail.com>
2019-06-20 20:47:49 +0300

    ייי

--- js/cpms/email/email-filter.cmp.js
+++ js/cpms/email/email-filter.cmp.js
@@ -2,7 +2,7 @@ export default {
     template: `
         <section class="email-filter-container">
         <input type="text" autofocus placeholder="search" v-model="filterBy.title"  @input="emitFilter"  />
-        <select v-model="filterBy.isRead"  >
+        <select v-model="filterBy.isRead" @change="emitFilter($event)"  >
         <option>All</option>
                          <option>Read</option>
                          <option>UnRead</option>
@@ -19,7 +19,8 @@ export default {
         }
     },
     methods: {
-        emitFilter() {
+        emitFilter(event) {
+            this.filterBy.isRead = event.target.value
             console.log(this.filterBy)
             this.$emit('filtered', this.filterBy);
 
